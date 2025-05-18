6752014eb8b932a5593be21e3a07cd83436b8e88
conmeobeo121 <kiendeptrai2002@gmail.com>
2025-05-18 09:59:33 +0700

    lib

--- src/main/java/com/microservices/config/ProfileFilterAutoConfiguration.java
+++ src/main/java/com/microservices/config/ProfileFilterAutoConfiguration.java
@@ -1,14 +1,20 @@
 package com.microservices.config;
 
 import com.microservices.ProfileFilter;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.web.server.WebFilter;
 
-@Configuration
-@ConditionalOnWebApplication
+@AutoConfiguration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+@ConditionalOnClass({WebFilter.class, ReactiveSecurityContextHolder.class})
 public class ProfileFilterAutoConfiguration {
     @Bean
+    @ConditionalOnMissingBean(ProfileFilter.class)
     public ProfileFilter profileFilter() {
         return new ProfileFilter();
     }
---
+++ src/main/java/com/microservices/config/ProfileFilterProperties.java
@@ -0,0 +1,7 @@
+//package com.microservices.config;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+//@ConfigurationProperties(prefix = "microservices.profile-filter-reactive")
+//public class ProfileFilterProperties {
+//}
